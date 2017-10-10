a3ab7ad70e9fddf31f6b2b43a63c7c6468c0c66d
David Bierma <31113045+db2147@users.noreply.github.com>
2017-10-10 18:02:26 -0500

    <beg-javascript>

--- assets/javascript/index_html.js
+++ assets/javascript/index_html.js
@@ -1,35 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Crystal Game</title>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+	<title></title>
 </head>
-
 <body>
 
 	<div class="container">
-		<h1 class="text-center">Crystals Game</h1>
+		<h1 class= "text-center">Crystals Game</h1>
 
 		<div class="row">
-			
+
 			<div class="col-sm-6 text-center">
 				<h1 id="targetNumber">Target Number: </h1>
 			</div>
-			
+
 			<div class="col-sm-6 text-center">
 				<h1 id="totalNumber">Total: </h1>
 			</div>
 
-		</div> <!-- end row -->
+		</div> <!-- end row-->
 
-		<div class="row"> <!-- for smaller columns -->
+		<div class="row">
 
 			<div class="col-sm-3 text-center">
 				<img id="blueCrystal" class="crystal" src="assets/images/blue.png">
@@ -47,19 +41,16 @@
 				<img id="yellowCrystal" class="crystal" src="assets/images/yellow.png">
 			</div>
 
-		</div> <!-- end row -->
+		</div> <!-- end row-->
 
 		<div class="row">
 			<div class="col-sm-2 col-md-offset-5 text-center bg-primary sDown">
-				<h2 id="sWins">Wins: </h2>
-				<h2 id="sLoses">Losses: </h2>
-			</div>
+					<h2 id="sWins">Wins: </h2>
+					<h2 id="sLoses">Losses: </h2>
+			</div>			
 		</div>
 
-	</div> //this div is ending the container
-	
-
-
+	</div>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
--- assets/javascript/javascript_js.js
+++ assets/javascript/javascript_js.js
@@ -3,12 +3,12 @@
 // 4 crystals, each have a random value between 1 and 12
 // wins and loses
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 	var redNum = 0;
 	var greenNum = 0;
 	var blueNum = 0;
-	var yellowNUm = 0;
+	var yellowNum = 0;
 
 	var sumCrystals = 0;
 	var clickColor = "";
@@ -20,13 +20,12 @@ $(document).ready(function(){
 	redNum = Math.floor(Math.random() * 12) + 1;
 	greenNum = Math.floor(Math.random() * 12) + 1;
 	blueNum = Math.floor(Math.random() * 12) + 1;
-	yellowNUm = Math.floor(Math.random() * 12) + 1;
+	yellowNum = Math.floor(Math.random() * 12) + 1;
 
 
-	$(".crystal").on("click"), function(){
+	$(".crystal").on('click', function() {
 		console.log('I am within the crystal on click function');
 
-		}); //not sure why lighting up
-	
 	});
-}
+
+});
