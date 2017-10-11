7351ee26c63bddcdb1133a87ff2e410be62dea00
David Bierma <31113045+db2147@users.noreply.github.com>
2017-10-10 21:21:32 -0500

    <id-tags>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	var sumCrystals = 0;
 	var clickColor = "";
 	var wins = 0;
-	var loses = 0;
+	var losses = 0;
 
 	var targetNum = 0;
 
@@ -21,7 +21,7 @@ $(document).ready(function(){
 	greenNum = Math.floor(Math.random() * 12) + 1;
 	blueNum = Math.floor(Math.random() * 12) + 1;
 	yellowNUm = Math.floor(Math.random() * 12) + 1;
-
+		//generating the random number for each of the crystals
 
 	$(".crystal").on("click"), function(){
 		console.log('I am within the crystal on click function');
